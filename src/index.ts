23bc66656d2a592fbd8a8589b36198426c6b7927
Alessandro <eusantosdev@gmail.com>
2025-02-06 11:30:36 -0300

    Servidor modificado!

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { routes } from './routes';
 
 const app = express();
+
 const port = 3000;
+const hostname = "127.0.0.1";
 
 routes(app);
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://${hostname}:${port}`);
 });
